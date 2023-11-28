1d93b5ebfb2e5d720671d4f6d429aa1f70e60573
Allan Pead <adpead@gmail.com>
2023-11-28 11:56:39 +0200

    First Commit

---
+++ ClassesExercises/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ClassesExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 4, 4, 6, 6 };
+            
+            bool isOddNumber = OddNumbers.CheckOddNumbers(numbers);
+
+            if (isOddNumber)
+            {
+                Console.WriteLine("The array contains an odd number");
+            }
+            else
+            {
+                Console.WriteLine("The array does not contain an odd number");
+
+            }
+
+            string letters = "Today is Tuesday";
+
+            string removedVowelsString = Vowels.RemoveAllTheVowels(letters);
+            Vowels.
+            Console.WriteLine(removedVowelsString);
+
+        }
+    }
+}
---
+++ ClassesExercises/Vowels.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercises
+{
+    internal class Vowels
+    {
+        private static bool IsAVowel(char letter)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            for (int index = 0; index < vowels.Length; index++)
+            {
+                if (letter == vowels[index])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        public static string RemoveAllTheVowels(string letters)
+        {
+            string returnString = "";
+
+            for (int index = 0; index < letters.Length; index++)
+            {
+                char letter = letters[index];
+                bool vowel = IsAVowel(letter);
+
+                if (!vowel)
+                {
+                    returnString += letter;
+                }
+
+
+            }
+
+            return returnString;
+        }
+
+
+
+    }
+}
